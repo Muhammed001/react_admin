e8b2b785befcbc71f0dec652d974ca0bbbd2de5f
e <happybbd@163.com>
2019-10-24 23:30:20 +0800

    框架初始化

---
+++ src/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');//导入 在页面中自动生成html页面的插件
+//创建一个插件的实例对象
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, './src/index.html'),//源文件
+    filename: 'index.html'//生成内存中首页的名称
+});
+
+// 向外暴露一个打包的配置对象（node语法）  webpack是基于Node构建的，说以wenpack支持所有的Node Api we和语法
+module.exports = {
+    mode: 'development',
+    plugins: [
+        htmlPlugin
+    ]
+};
